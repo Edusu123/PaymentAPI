fc7a84a79d5470de21b704fd400ef076aef021a2
Eduardo Conde Pires <eduardo.c.pires@outlook.com>
2022-09-20 10:55:51 -0300

    Define EF Model & DB Context class

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        PaymentDetailContext(DbContextOptions<PaymentDetailContext> paymentDetailContext) : base(paymentDetailContext) { }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
--- PaymentAPI/Startup.cs
+++ PaymentAPI/Startup.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PaymentAPI.Models;
 
 namespace PaymentAPI
 {
@@ -22,15 +18,16 @@ namespace PaymentAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
             });
+
+            services.AddDbContext<PaymentDetailContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
